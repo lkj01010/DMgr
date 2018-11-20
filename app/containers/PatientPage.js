917749fc2bd51a79bd8d1d6a211ce22b111c07aa
Midstream <lkj01010@163.com>
2018-11-21 03:56:44 +0800

    +: layout

--- app/containers/PatientPage.js
+++ app/containers/PatientPage.js
@@ -1,74 +1,73 @@
 // @flow
 import React, { Component } from 'react';
 
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Button, Layout, Menu, Breadcrumb, Icon, Row, Col, Divider, Input,
+    Tooltip
+} from 'antd';
 
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
 
+import styles from './PatientPage.css';
+
 type Props = {};
+const Search = Input.Search;
+
+
+const data = [{
+    key: '1',
+    firstName: 'John',
+    lastName: 'Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  }, {
+    key: '2',
+    firstName: 'Jim',
+    lastName: 'Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  }, {
+    key: '3',
+    firstName: 'Joe',
+    lastName: 'Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  }];
+
+
 
 export default class PatientPage extends Component<Props> {
     props: Props;
 
     render() {
-        return <Layout>
-            {/* <Header className="header">
-                <div className="logo" />
-                <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    defaultSelectedKeys={['2']}
-                    style={{ lineHeight: '64px' }}
-                    >
-                    <Menu.Item key="1">nav 1</Menu.Item>
-                    <Menu.Item key="2">nav 2</Menu.Item>
-                    <Menu.Item key="3">nav 3</Menu.Item>
-                </Menu>
-            </Header>
-            <Content style={{ padding: '0 50px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
-                    <Breadcrumb.Item>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item>List</Breadcrumb.Item>
-                    <Breadcrumb.Item>App</Breadcrumb.Item>
-                </Breadcrumb>
-                <Layout style={{ padding: '24px 0', background: '#fff' }}>
-                    <Sider width={200} style={{ background: '#fff' }}>
-                        <Menu
-                        mode="inline"
-                        defaultSelectedKeys={['1']}
-                        defaultOpenKeys={['sub1']}
-                        style={{ height: '100%' }}
-                        >
-                            <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
-                                <Menu.Item key="1">option1</Menu.Item>
-                                <Menu.Item key="2">option2</Menu.Item>
-                                <Menu.Item key="3">option3</Menu.Item>
-                                <Menu.Item key="4">option4</Menu.Item>
-                            </SubMenu>
-                            <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-                                <Menu.Item key="5">option5</Menu.Item>
-                                <Menu.Item key="6">option6</Menu.Item>
-                                <Menu.Item key="7">option7</Menu.Item>
-                                <Menu.Item key="8">option8</Menu.Item>
-                            </SubMenu>
-                            <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-                                <Menu.Item key="9">option9</Menu.Item>
-                                <Menu.Item key="10">option10</Menu.Item>
-                                <Menu.Item key="11">option11</Menu.Item>
-                                <Menu.Item key="12">option12</Menu.Item>
-                            </SubMenu>
-                        </Menu>
-                    </Sider>
-                    <Content style={{ padding: '0 24px', minHeight: 280 }}>
-                        Content
-                    </Content>
-                </Layout>
+        return (
+            <Content>
+                <Row type="flex" justify="space-between" style={{height: '100%'}}>
+                
+                    <Col span={12} className={styles.leftMainCtn}>
+                        <Row type="flex" justify="end" style={{background: '#ddd', padding: 16}}>
+                        <Tooltip placement="bottomLeft" title={'搜索语法提示'}>
+                            <Search
+                                style={{marginRight: 16, width: 200}}
+                                placeholder="id搜索"
+                                onSearch={value => console.log(value)}
+                                enterButton
+                            />
+                        </Tooltip>
+                            <Button type="primary" style={{marginRight: 8}}>新建档案</Button>
+                        </Row>
+                    </Col>
+                    {/* <Divider type="vertical"></Divider> */}
+                    <Col span={12} className={styles.rightMainCtn}>
+                        hello
+                    </Col>
+                </Row>
             </Content>
-            <Footer style={{ textAlign: 'center' }}>
-              Ant Design ©2018 Created by Ant UED
-            </Footer> */}
-        </Layout>;
+            
+        )
     }
 }
 
---
+++ app/containers/Routes.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router';
+import routes from '../constants/routes';
+import App from './App';
+import HomePage from './HomePage';
+import CounterPage from './CounterPage';
+import PatientPage from './PatientPage';
+
+import { Link } from 'react-router-dom';
+
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu;
+
+
+
+export default class Routes extends Component<Props> {
+    state = {
+        collapsed: true,
+    };
+    
+    onCollapse = (collapsed) => {
+        console.log(collapsed);
+        this.setState({ collapsed });
+    };
+
+    render() {
+        return (
+            <App>
+                <Layout style={{ minHeight: '100vh' }}>
+                    <Sider
+                        collapsible={false}
+                        collapsed={this.state.collapsed}
+                        onCollapse={this.onCollapse}
+                    >
+                        <div className="logo" />
+                        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                            <Menu.Item key="1">
+                            <Icon type="profile" />
+                            {/* <span>随访表</span> */}
+                            <Link to={routes.PATIENT}>随访表</Link>
+                            </Menu.Item>
+                            <Menu.Item key="2">
+                            <Icon type="dashboard" />
+                            <Link to={routes.COUNTER}>待扩展</Link>
+                            </Menu.Item>
+                        </Menu>
+                    </Sider>
+
+                    <Switch>
+                        <Route path={routes.COUNTER} component={CounterPage} />
+                        <Route path={routes.HOME} component={HomePage} />
+                        <Route path={routes.PATIENT} component={PatientPage} />
+                    </Switch>
+
+                {/* <Footer style={{ textAlign: 'center' }}>
+                        Ant Design ©2018 Created by Ant UED
+                </Footer> */}
+                </Layout>
+            </App>
+        );
+        
+    }
+    
+};
